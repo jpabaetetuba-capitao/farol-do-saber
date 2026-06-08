1f25d9e67c552ef11f616a6835f4e624ca2e3986
jpabaetetuba-capitao <jpabaetetuba@gmail.com>
2026-06-08 01:45:54 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBvgEA-hHHNiC3npzL8JnnYTogyn6zg3Fs",
+
+  authDomain: "farol-do-saber.firebaseapp.com",
+
+  projectId: "farol-do-saber",
+
+  storageBucket: "farol-do-saber.firebasestorage.app",
+
+  messagingSenderId: "24326073020",
+
+  appId: "1:24326073020:web:90507b1ca34b6c91e53eb9"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const db = firebase.firestore();
