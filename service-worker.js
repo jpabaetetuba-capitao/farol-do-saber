ea971c700d85c3d770909ea36d3c0a66c0eb3112
jpabaetetuba-capitao <jpabaetetuba@gmail.com>
2026-08-04 16:41:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "farol-v65-2026-08-04";
+const CACHE_VERSION = "farol-v66-2026-08-04";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
